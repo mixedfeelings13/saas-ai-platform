c0a813478eadbcacc349012a572467a934840723
mixedfeelings13 <fuegodjh@gmail.com>
2023-10-18 09:14:14 +0100

    Added OpenAi Configuration for conversation

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -58,7 +58,7 @@ const DashboardPage = () => {
         <div className="px-4 md:px-20 lg:px-32 space-y-4">
           {tools.map((tool) => (
             <Card
-            onClick={() => router.push(tool.href)}
+              onClick={() => router.push(tool.href)}
               key={tool.href}
               className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
             >
@@ -67,7 +67,7 @@ const DashboardPage = () => {
                   <tool.icon className={cn("w-8 h-8", tool.color)} />
                 </div>
                 <div className="font-semibold">
-                  <tool.label />
+                  {tool.label}
                 </div>
               </div>
               <ArrowRight className="w-5 h-5" />
